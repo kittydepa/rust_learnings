db8e5ab1c2f7f89210a104f512c9d0d086d722b3
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-02 19:22:50 +0100

    variables

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+
+
+// fn main() {
+//     let x = 5.0;
+    
+//     let x =  x + 1.0;
+
+//     let x = x * 2.0;
+
+//     println!("The value of x is: {}", x);
+// }
+
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is {}", y);
+}
+
---
+++ src/tuples.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is {}", y);
+}
+
+// fn poop() {
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+// }
+
+// This is a tuple. Itis a comma-separate list of values inside parentheses
+
+
+// To get individual values out of a tuple, use patern matching:
